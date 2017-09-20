ec38cd3f15310b9e074ba1d0067532b8717569d4
nate-d-olson <nathandavidolson@gmail.com>
2017-09-20 13:14:42 -0400

    added unclustered

--- lib/get_mgtst_objects.R
+++ lib/get_mgtst_objects.R
@@ -5,7 +5,8 @@ get_mrexp <- function(pipelines_dir){
       mrexp_files <- list(
             dada2 = file.path(pipeline_dir, "dada2/dada_mrexp.rds"),
             mothur =  file.path(pipeline_dir, "mothur/mothur_mrexp.rds"),
-            qiime =  file.path(pipeline_dir, "qiime/qiime_mrexp.rds")
+            qiime =  file.path(pipeline_dir, "qiime/qiime_mrexp.rds"),
+            unclustered =  file.path(pipeline_dir, "mothur/unclustered_mrexp.rds")
       )
       
       mrexp_files %>% map(readRDS)
@@ -19,7 +20,8 @@ get_phyloseq <- function(pipelines_dir){
       ps_files <- list(
             dada2 = file.path(pipeline_dir, "dada2/dada_ps.rds"),
             mothur =  file.path(pipeline_dir, "mothur/mothur_ps.rds"),
-            qiime =  file.path(pipeline_dir, "qiime/qiime_ps.rds")
+            qiime =  file.path(pipeline_dir, "qiime/qiime_ps.rds"),
+            unclustered =  file.path(pipeline_dir, "mothur/unclustered_ps.rds")
       )
       
       ps_files %>% map(readRDS)
